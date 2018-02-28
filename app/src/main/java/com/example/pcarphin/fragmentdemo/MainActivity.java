55acc038a732e7d393cf5a128c0b1e10cf7347f5
Philippe Carphin <philippe.carphin@polymtl.ca>
2018-02-27 20:11:46 -0500

    Get reference to my fragment in ActivityMain.java
    Note the changed import statement in the fragment class and the comment in
    MainActivity

--- app/src/main/java/com/example/pcarphin/fragmentdemo/MainActivity.java
+++ app/src/main/java/com/example/pcarphin/fragmentdemo/MainActivity.java
@@ -20,9 +20,15 @@ public class MainActivity extends AppCompatActivity {
             )
     };
 
+    private MenuDetailFragment menuDetailFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // see https://stackoverflow.com/questions/27037662/incompatible-types-homefragment-cannot-be-converted-to-fragment-in-android
+        // I could have changed this to getSupportFragmentManager() but I did like in the Udemy course
+        // and changed the import in the fragment class.
+        menuDetailFragment = (MenuDetailFragment) getFragmentManager().findFragmentById(R.id.detail_frag);
     }
 }
--- app/src/main/java/com/example/pcarphin/fragmentdemo/MenuDetailFragment.java
+++ app/src/main/java/com/example/pcarphin/fragmentdemo/MenuDetailFragment.java
@@ -2,7 +2,7 @@ package com.example.pcarphin.fragmentdemo;
 
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
