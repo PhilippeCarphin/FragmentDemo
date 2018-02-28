2a926551d745a4a5a10a5f2ad0f2f3d39a2e4f6e
Philippe Carphin <philippe.carphin@polymtl.ca>
2018-02-27 19:57:02 -0500

    Added MenuItem class and list of MenuItem in MainActivity

--- app/src/main/java/com/example/pcarphin/fragmentdemo/MainActivity.java
+++ app/src/main/java/com/example/pcarphin/fragmentdemo/MainActivity.java
@@ -5,6 +5,21 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final MenuItem[] menu = {
+            new MenuItem(
+                    "Breakfast",
+                    "2 eggs bacon\nserved with potatoes\ntoast\nand coffee"
+            ),
+            new MenuItem(
+                    "Breakfast",
+                    "2 eggs bacon\nserved with potatoes\ntoast\nand coffee"
+            ),
+            new MenuItem(
+                    "Breakfast",
+                    "2 eggs bacon\nserved with potatoes\ntoast\nand coffee"
+            )
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
---
+++ app/src/main/java/com/example/pcarphin/fragmentdemo/MenuItem.java
@@ -0,0 +1,17 @@
+package com.example.pcarphin.fragmentdemo;
+
+/**
+ * Created by pcarphin on 2018-02-27.
+ */
+
+public class MenuItem {
+
+    private String name;
+    private String description;
+
+    public MenuItem(String name, String description){
+        this.name = name;
+        this.description = description;
+    }
+
+}
