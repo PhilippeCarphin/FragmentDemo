e9bdfdf97aee9b40cb6b4a96ef7c460985c28242
Philippe Carphin <philippe.carphin@polymtl.ca>
2018-02-27 20:45:58 -0500

    Added ListView fragment to the thing
    No connection has been made

--- app/src/main/java/com/example/pcarphin/fragmentdemo/MenuListFragment.java
+++ app/src/main/java/com/example/pcarphin/fragmentdemo/MenuListFragment.java
@@ -3,15 +3,16 @@ package com.example.pcarphin.fragmentdemo;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MenuListFragment extends Fragment {
+public class MenuListFragment extends ListFragment {
 
 
     public MenuListFragment() {
@@ -22,9 +23,20 @@ public class MenuListFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        TextView textView = new TextView(getActivity());
-        textView.setText(R.string.hello_blank_fragment);
-        return textView;
+        MenuItem[] menu = MainActivity.menu;
+        String names[] = new String[menu.length];
+        for(int i = 0; i < names.length; ++i){
+            names[i] = menu[i].getName();
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                inflater.getContext(),
+                android.R.layout.simple_list_item_1,
+                names
+        );
+
+        setListAdapter(adapter);
+
+        return super.onCreateView(inflater, container, savedInstanceState);
     }
 
 }
